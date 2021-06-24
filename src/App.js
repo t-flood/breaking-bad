98551a078de0aa09654a58d398f8e2874bff307c
Tim Flood <tflood9@gmail.com>
2021-06-23 20:02:57 -0400

    completed search functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Header from "./components/ui/Header";
 import axios from "axios";
-import CharacterGrid from "./components/ui/characters/CharacterGrid";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 const App = () => {
   const [items, setItems] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters
+      const result =
+        await axios(`https://www.breakingbadapi.com/api/characters?name=${query}
 
       `);
       console.log(result.data);
@@ -18,10 +21,11 @@ const App = () => {
       setIsLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(query) => setQuery(query)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
